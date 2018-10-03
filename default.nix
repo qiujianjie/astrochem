abf048c856e9674867ab0bb6495aa371a9e66292
Sebastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2018-10-03 15:23:38 +0200

    Freeze sundials version to 2.7.0 in default.nix

--- default.nix
+++ default.nix
@@ -1,9 +1,30 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
+
   name = "astrochem-env";
+
   env = buildEnv { name = name; paths = buildInputs; };
-  python2Env = python2.withPackages (ps: with ps; [ numpy h5py ]);
+
+  python2Env = python2.withPackages (ps: with ps; [ numpy h5py cython ]);
+
+  sundials2 = stdenv.mkDerivation rec {
+    pname = "sundials";
+    version = "2.7.0";
+    name = "${pname}-${version}";
+
+    src = fetchurl {
+      url = "https://computation.llnl.gov/projects/${pname}/download/${pname}-${version}.tar.gz";
+      sha256 = "01513g0j7nr3rh7hqjld6mw0mcx5j9z9y87bwjc16w2x2z3wm7yk";
+    };
+
+    preConfigure = ''
+      export cmakeFlags="-DCMAKE_INSTALL_PREFIX=$out -DEXAMPLES_INSTALL_PATH=$out/share/examples $cmakeFlags"
+    '';
+
+    buildInputs = [ cmake python ];
+  };
+
   buildInputs = [
     python2Env
     gfortran
@@ -11,6 +32,7 @@ stdenv.mkDerivation rec {
     autoconf
     automake
     libtool
-    sundials
+    sundials2
   ];
+
 }
