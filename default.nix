d3affd2613fdd8bfdc76ed0436cd8ff5c2a3b2c5
Sebastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2018-10-04 10:37:38 +0200

    Simplify sundials derivation in default.nix

--- default.nix
+++ default.nix
@@ -8,22 +8,13 @@ stdenv.mkDerivation rec {
 
   python2Env = python2.withPackages (ps: with ps; [ numpy h5py cython ]);
 
-  sundials2 = stdenv.mkDerivation rec {
-    pname = "sundials";
-    version = "2.7.0";
-    name = "${pname}-${version}";
-
+  sundials2 = sundials.overrideAttrs (oldAttrs: rec {
+    name = "sundials-2.7.0";
     src = fetchurl {
-      url = "https://computation.llnl.gov/projects/${pname}/download/${pname}-${version}.tar.gz";
+      url = "https://computation.llnl.gov/projects/sundials/download/sundials-2.7.0.tar.gz";
       sha256 = "01513g0j7nr3rh7hqjld6mw0mcx5j9z9y87bwjc16w2x2z3wm7yk";
     };
-
-    preConfigure = ''
-      export cmakeFlags="-DCMAKE_INSTALL_PREFIX=$out -DEXAMPLES_INSTALL_PATH=$out/share/examples $cmakeFlags"
-    '';
-
-    buildInputs = [ cmake python ];
-  };
+  });
 
   buildInputs = [
     python2Env
